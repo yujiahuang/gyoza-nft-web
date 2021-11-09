86e9d95f3e2face613075a0306c20260e7d1b0e6
Yuji <kevinhuangdreamer@gmail.com>
2021-11-09 09:01:00 +0800

    [feat] render a basic sphere

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,20 @@
 import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import ReactGA from "react-ga4";
-import { parseISO, format } from 'date-fns';
-
-// const {  } = process.env;
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 function Home() {
 
   return (
     <div className="home">
-
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <mesh visible rotation={[Math.PI / 2, 0, 0]}>
+          <sphereGeometry args={[1, 32, 16]} />
+          <meshBasicMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
